0f33b7ced7225b018f56432d84a49f5077d24a75
kse-music <dh.hiekn@gmail.com>
2018-10-15 23:00:22 +0800

    zuul

--- gateway/src/main/java/com/hiekn/metacloud/gateway/GateWayApplication.java
+++ gateway/src/main/java/com/hiekn/metacloud/gateway/GateWayApplication.java
@@ -1,20 +1,14 @@
 package com.hiekn.metacloud.gateway;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.core.env.Environment;
 
 @EnableZuulProxy
 @EnableEurekaClient
 @SpringBootApplication
 public class GateWayApplication {
-
-
-    @Autowired
-    Environment environment;
 
     public static void main(String[] args) {
         SpringApplication.run(GateWayApplication.class, args);
