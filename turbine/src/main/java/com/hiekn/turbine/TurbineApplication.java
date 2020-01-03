ae6f2a7c3441a3cb52dcd8e9677d977266e80ade
DingHao <dh.hiekn@gmail.com>
2020-01-03 20:45:11 +0800

    fix pom

--- turbine/src/main/java/com/hiekn/turbine/TurbineApplication.java
+++ turbine/src/main/java/com/hiekn/turbine/TurbineApplication.java
@@ -2,7 +2,6 @@ package com.hiekn.turbine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
