7be5a6141325566e8c560443cf4afc8bd6bb95a9
dinghao <dinghao@hiek.com>
2020-12-23 13:49:31 +0800

    2.4.1

--- web1/src/main/java/cn/hiboot/web/WebApplication.java
+++ web1/src/main/java/cn/hiboot/web/WebApplication.java
@@ -1,11 +1,7 @@
 package cn.hiboot.web;
 
-import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
@@ -14,24 +10,11 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  * @author DingHao
  * @since 2019/12/29 0:01
  */
-@EnableHystrix
-@EnableHystrixDashboard
 @EnableFeignClients(basePackages = "com.hiekn.metacloud.feign")
 @SpringBootApplication
 public class WebApplication {
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
-    }
-
-//    @Bean
-    public ServletRegistrationBean<HystrixMetricsStreamServlet> getServlet(){
-        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
-        ServletRegistrationBean<HystrixMetricsStreamServlet> registrationBean = new ServletRegistrationBean<>(streamServlet);
-        registrationBean.setLoadOnStartup(1);
-        // /actuator/hystrix.stream 可以直接使用这个?
-        registrationBean.addUrlMappings("/hystrix.stream");
-        registrationBean.setName("HystrixMetricsStreamServlet");
-        return registrationBean;
     }
 
 }
