6e9a62fd65d0f4d652ccba1772d952beea80eabf
DingHao <dh.hiekn@gmail.com>
2021-07-02 22:45:58 +0800

    add DingTalkNotifier

--- monitor/src/main/java/com/hiekn/metacloud/monitor/DingTalkNotifier.java
+++ monitor/src/main/java/com/hiekn/metacloud/monitor/DingTalkNotifier.java
@@ -3,31 +3,76 @@ package com.hiekn.metacloud.monitor;
 import de.codecentric.boot.admin.server.domain.entities.Instance;
 import de.codecentric.boot.admin.server.domain.entities.InstanceRepository;
 import de.codecentric.boot.admin.server.domain.events.InstanceEvent;
-import de.codecentric.boot.admin.server.notify.AbstractStatusChangeNotifier;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.stereotype.Component;
+import de.codecentric.boot.admin.server.domain.events.InstanceStatusChangedEvent;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.util.StringUtils;
 import org.springframework.web.client.RestTemplate;
-import reactor.core.publisher.Mono;
 
-@ConditionalOnProperty(
-        prefix = "spring.boot.admin.notify.dingtalk",
-        name = {"webhook-token"}
-)
-@Component
-public class DingTalkNotifier extends AbstractStatusChangeNotifier {
+import java.util.HashMap;
+import java.util.Map;
 
-    private final RestTemplate restTemplate = new RestTemplate();
+public class DingTalkNotifier extends de.codecentric.boot.admin.server.notify.DingTalkNotifier {
 
-    private final DingTalkNotifierConfiguration configuration;
+    private String atMobiles;
 
-    public DingTalkNotifier(DingTalkNotifierConfiguration configuration,InstanceRepository repository) {
-        super(repository);
-        this.configuration = configuration;
+    public DingTalkNotifier(InstanceRepository repository) {
+        super(repository,new RestTemplate());
+    }
+
+    public String getAtMobiles() {
+        return atMobiles;
+    }
+
+    public void setAtMobiles(String atMobiles) {
+        this.atMobiles = atMobiles;
     }
 
     @Override
-    protected Mono<Void> doNotify(InstanceEvent event, Instance instance) {
-        return Mono.fromRunnable(() -> restTemplate.postForEntity(configuration.getWebhookToken(), configuration.create(event, instance),Void.class));
+    protected Object createMessage(InstanceEvent event, Instance instance) {
+        String msg = "";
+        if(event instanceof InstanceStatusChangedEvent){//只关心状态变化
+            InstanceStatusChangedEvent changedEvent = (InstanceStatusChangedEvent) event;
+            StringBuilder sb = new StringBuilder("\n >**");
+            sb.append(instance.getRegistration().getName()).append("** 服务已经");
+            if(changedEvent.getStatusInfo().isOffline()){
+                sb.append("Offline,请相关人员及时去救火 \n > ![screenshot](http://cdn.hiboot.cn/36fdcb749ffd4f7ab19b1c1daab4c9ef.jpg)");
+            }else if(changedEvent.getStatusInfo().isDown()){
+                sb.append("Down,请相关人员及时去救火 \n > ![screenshot](http://cdn.hiboot.cn/36fdcb749ffd4f7ab19b1c1daab4c9ef.jpg)");
+            }else if(changedEvent.getStatusInfo().isUp()){
+                sb.append("又重新复活了 \n > ![screenshot](http://cdn.hiboot.cn/cb967d6e0d634fc18d684e87153791e8.jpg)");
+            }
+            if(this.atMobiles != null){
+                sb.append(this.getAtMobilesString(this.atMobiles));
+            }
+            msg = sb.toString();
+        }
+        Map<String, Object> messageJson = new HashMap<>();
+        Map<String, String> params = new HashMap<>();
+        Map<String, Object> at = new HashMap<>();
+        params.put("text", msg);
+        String title = "服务通知";
+        params.put("title", title);
+        at.put("atMobiles", StringUtils.commaDelimitedListToStringArray(this.atMobiles));
+        at.put("isAtAll",false);
+        messageJson.put("at", at);
+        String msgtype = "markdown";
+        messageJson.put("msgtype", msgtype);
+        messageJson.put(msgtype, params);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        return new HttpEntity<>(messageJson, headers);
     }
+
+    private String getAtMobilesString(String s) {
+        StringBuilder atMobiles = new StringBuilder();
+        String[] mobiles = s.split(",");
+        for (String mobile : mobiles) {
+            atMobiles.append("@").append(mobile);
+        }
+        return atMobiles.toString();
+    }
+
 
 }
--- monitor/src/main/java/com/hiekn/metacloud/monitor/DingTalkNotifierConfiguration.java
+++ monitor/src/main/java/com/hiekn/metacloud/monitor/DingTalkNotifierConfiguration.java
@@ -1,76 +1,19 @@
 package com.hiekn.metacloud.monitor;
 
-import de.codecentric.boot.admin.server.domain.entities.Instance;
-import de.codecentric.boot.admin.server.domain.events.InstanceEvent;
-import de.codecentric.boot.admin.server.domain.events.InstanceStatusChangedEvent;
-import lombok.Getter;
-import lombok.Setter;
+import de.codecentric.boot.admin.server.domain.entities.InstanceRepository;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.ConstructorBinding;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.util.StringUtils;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-import java.util.HashMap;
-import java.util.Map;
-
-@ConfigurationProperties(prefix = "spring.boot.admin.notify.dingtalk")
-@Setter
-@Getter
+@Configuration(proxyBeanMethods = false)
+@ConditionalOnProperty(prefix = "spring.boot.admin.notify.dingtalk", name = "webhook-url")
 public class DingTalkNotifierConfiguration {
 
-    private String webhookToken;
-    private String atMobiles;
-    private String msgtype = "markdown";
-    private String title = "服务通知";
-
-    @ConstructorBinding
-    public DingTalkNotifierConfiguration(String atMobiles,String webhookToken) {
-        this.atMobiles = atMobiles;
-        this.webhookToken = webhookToken;
-    }
-
-    public HttpEntity<Map<String, Object>> create(InstanceEvent event, Instance instance) {
-        String msg = "";
-        if(event instanceof InstanceStatusChangedEvent){//只关心状态变化
-            InstanceStatusChangedEvent changedEvent = (InstanceStatusChangedEvent) event;
-            StringBuilder sb = new StringBuilder("\n >**");
-            sb.append(instance.getRegistration().getName()).append("** 服务已经");
-            if(changedEvent.getStatusInfo().isOffline()){
-                sb.append("Offline,请相关人员及时去救火 \n > ![screenshot](http://cdn.hiboot.cn/36fdcb749ffd4f7ab19b1c1daab4c9ef.jpg)");
-            }else if(changedEvent.getStatusInfo().isDown()){
-                sb.append("Down,请相关人员及时去救火 \n > ![screenshot](http://cdn.hiboot.cn/36fdcb749ffd4f7ab19b1c1daab4c9ef.jpg)");
-            }else if(changedEvent.getStatusInfo().isUp()){
-                sb.append("又重新复活了 \n > ![screenshot](http://cdn.hiboot.cn/cb967d6e0d634fc18d684e87153791e8.jpg)");
-            }
-            if(this.atMobiles != null){
-                sb.append(this.getAtMobilesString(this.atMobiles));
-            }
-            msg = sb.toString();
-        }
-        Map<String, Object> messageJson = new HashMap<>();
-        Map<String, String> params = new HashMap<>();
-        Map<String, Object> at = new HashMap<>();
-        params.put("text", msg);
-        params.put("title", this.title);
-        at.put("atMobiles", StringUtils.commaDelimitedListToStringArray(this.atMobiles));
-        at.put("isAtAll",false);
-        messageJson.put("at", at);
-        messageJson.put("msgtype", this.msgtype);
-        messageJson.put(this.msgtype, params);
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        return new HttpEntity<>(messageJson, headers);
-    }
-
-    private String getAtMobilesString(String s) {
-        StringBuilder atMobiles = new StringBuilder();
-        String[] mobiles = s.split(",");
-        for (String mobile : mobiles) {
-            atMobiles.append("@").append(mobile);
-        }
-        return atMobiles.toString();
+    @Bean
+    @ConfigurationProperties("spring.boot.admin.notify.dingtalk")
+    public DingTalkNotifier dingTalkNotifier(InstanceRepository repository) {
+        return new DingTalkNotifier(repository);
     }
 
 }
