e59a593b9c024c8d4838bf11d56a3e41468919c3
DingHao <dh.hiekn@gmail.com>
2021-07-05 12:55:45 +0800

    remove gateway fallback

--- gateway/src/main/java/com/hiekn/metacloud/gateway/GateWayApplication.java
+++ gateway/src/main/java/com/hiekn/metacloud/gateway/GateWayApplication.java
@@ -1,23 +1,13 @@
 package com.hiekn.metacloud.gateway;
 
-import cn.hiboot.mcn.core.exception.ErrorMsg;
-import cn.hiboot.mcn.core.model.result.RestResp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class GateWayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GateWayApplication.class, args);
-    }
-
-    @RequestMapping("/fallback")
-    public RestResp fallback() {
-        return new RestResp<>(ErrorMsg.REMOTE_SERVICE_ERROR,ErrorMsg.getErrorMsg(ErrorMsg.REMOTE_SERVICE_ERROR));
     }
 
 }
